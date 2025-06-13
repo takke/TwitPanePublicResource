89df0dba9126bbae1c1a06744312e7be16d57c7f
takke <takke30@gmail.com>
2025-06-13 15:47:17 +0900

    TwitPanePublicResource に集約

---
+++ src/androidMain/kotlin/com/twitpane/core/PublicResourceAlias.kt
@@ -0,0 +1,7 @@
+package com.twitpane.core
+
+// KMP/CMP では PublicResourceStringRes を使うこと
+typealias PublicResourceString = com.twitpane.public_resource.R.string
+
+// KMP/CMP では PublicResourceArrayRes を使うこと
+typealias PublicResourceArray = com.twitpane.public_resource.R.array
---
+++ src/commonMain/kotlin/com/twitpane/core/PublicResourceResAlias.kt
@@ -0,0 +1,5 @@
+package com.twitpane.core
+
+typealias PublicResourceStringRes = twitpane.public_resources.resources.Res.string
+
+typealias PublicResourceArrayRes = twitpane.public_resources.resources.Res.array
