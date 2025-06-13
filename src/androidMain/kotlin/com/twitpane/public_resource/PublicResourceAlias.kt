9a31b374005f4d81100a551880b36667dc4ac4c5
takke <takke30@gmail.com>
2025-06-13 15:54:21 +0900

    com.twitpane.public_resource パッケージに変更

---
+++ src/androidMain/kotlin/com/twitpane/public_resource/PublicResourceAlias.kt
@@ -0,0 +1,7 @@
+package com.twitpane.public_resource
+
+// KMP/CMP では PublicResourceStringRes を使うこと
+typealias PublicResourceString = R.string
+
+// KMP/CMP では PublicResourceArrayRes を使うこと
+typealias PublicResourceArray = R.array
